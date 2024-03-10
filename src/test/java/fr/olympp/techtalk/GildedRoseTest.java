4b07b168bb67298b54d93f59fd88e49e23318461
xLillium <nicolas.motillon@gmail.com>
2024-03-10 15:32:59 +0100

    GoldenMaster: Increasing Tests Strength with some edgecases

--- src/test/java/fr/olympp/techtalk/GildedRoseTest.java
+++ src/test/java/fr/olympp/techtalk/GildedRoseTest.java
@@ -40,4 +40,46 @@ public class GildedRoseTest {
         }
     }
 
+    @Test
+    public void shouldIncreaseQualityByOneForAgedBrieJustBeforeSellInZero() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(11);
+    }
+
+    @Test
+    public void shouldDecreaseQualityTwiceAsFastWhenSellInBelowZero() {
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 0, 10) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(8);
+    }
+
+
+    @Test
+    public void shouldNotDecreaseQualityBelowZero() {
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(0);
+    }
+
+
+    @Test
+    public void shouldIncreaseBackstagePassesQualityByThreeWhenSellInBelowSix() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(23);
+    }
+
 }
