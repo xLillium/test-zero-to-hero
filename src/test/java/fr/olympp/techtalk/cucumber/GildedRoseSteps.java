b76e0040b0a9e84ebf3d4690f4ed03bde33ae67d
xLillium <nicolas.motillon@gmail.com>
2024-03-10 16:52:39 +0100

    BDD : Implementing the Steps with Cucumber

---
+++ src/test/java/fr/olympp/techtalk/cucumber/GildedRoseSteps.java
@@ -0,0 +1,36 @@
+package fr.olympp.techtalk.cucumber;
+
+import fr.olympp.techtalk.GildedRose;
+import fr.olympp.techtalk.Item;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GildedRoseSteps {
+    private Item[] items;
+    private GildedRose gildedRose;
+
+    @Given("a {string} with a sell-in of {int} and a quality of {int}")
+    public void a_with_a_sell_in_of_and_a_quality_of(String itemName, int sellIn, int quality) {
+        items = new Item[]{new Item(itemName, sellIn, quality)};
+        gildedRose = new GildedRose(items);
+    }
+
+    @When("a day passes")
+    public void aDayPasses() {
+        gildedRose.updateQuality();
+    }
+
+    @Then("its quality should be {int}")
+    public void itsQualityShouldBe(int expectedQuality) {
+        assertThat(items[0].quality).isEqualTo(expectedQuality);
+    }
+
+    @And("its sell-in should be {int}")
+    public void itsSellInShouldBe(int expectedSellin) {
+        assertThat(items[0].sellIn).isEqualTo(expectedSellin);
+    }
+}
